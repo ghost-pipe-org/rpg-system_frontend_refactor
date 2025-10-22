5707301275aa897d02387b03568f39b2bedc2310
Gaaaybe <gabenesus@gmail.com>
2025-10-22 14:12:38 -0300

    refactor: remove unused ProtectedRoute import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from 'react-router';
 import { AuthProvider } from './context/AuthContext';
 import { AppRouter } from './routes';
 import { DocumentTitle } from './components/DocumentTitle';
-import { ProtectedRoute } from './components/ProtectedRoute';
 function App() {
   return (
     <AuthProvider>
